411ed78a188023b718cd6046f140bd89868791c6
Alfred Lee <crazy.jser@gmail.com>
2013-06-07 23:56:31 +0800

    rm all files

--- README.md
+++
@@ -1,4 +0,0 @@
-div.li
-======
-
-The Open-Source for div.li website's developer.Use the Angularjs, nodejs, mongoDB, and an simple nodejs framework work on.
--- routes/admin.js
+++
@@ -1,6 +0,0 @@
-exports.init = function(req, res){
-	res.render('article', {
-		// title: "All about <p> Tags",
-		// body: "<p>This is a post about &lt;p&gt; tags</p>"
-	});
-};
